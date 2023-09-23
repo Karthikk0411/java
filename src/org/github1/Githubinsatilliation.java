e609d3eca3721a1da886031bea1458d55ea3ded5
karthikk0411 <karthikkumar.k0411@gmail.com>
2023-09-23 22:21:47 +0530

    karthik added code

--- src/org/github1/Githubinsatilliation.java
+++ src/org/github1/Githubinsatilliation.java
@@ -1,5 +1,17 @@
 package org.github1;
 
 public class Githubinsatilliation {
+	
+	private void kar() {
+
+		System.out.println("karthik");
+		System.out.println("kumar");
+	}
+	
+	public static void main(String[] args) {
+		
+		
+		
+	}
 
 }
